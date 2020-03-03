bb9e301c52a531f10ff73d191c30be23315924c5
lili gugushvili <lilikog@yahoo.com>
2020-03-03 13:20:12 -0500

    all files

---
+++ db/schema.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers(
+  id INT NOY NULL AUTO_INCREMENT,
+  burger_name VARCHAR (255) NOT NULL,
+  picked BOOLEAN DEFAULT false NOT NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO burgers(
+  (burger_name)
+  VALUES
+  ("Cheeseburger"),
+  ("Hamburger");
+  ("Turkey Burger"),
+  ("Veggie Burger"),
+  ("Salmon Burger")
+)
+
+SELECT * FROM burgers;
